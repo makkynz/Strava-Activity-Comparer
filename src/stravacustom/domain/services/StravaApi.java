5ba1a13bec509a8b8d173a0260bbe0b05be6b311
Anthony Marshall <anthonymarshall@gmail.com>
2020-04-01 15:28:30 +1300

    Capture strava oauth callback

---
+++ src/stravacustom/domain/services/StravaApi.java
@@ -0,0 +1,14 @@
+package stravacustom.domain.services;
+
+
+public class StravaApi {
+
+    public void getAuthorizationToken(){
+
+    }
+
+
+    public String getActivities(String stravaUserId){
+        return "";// TODO: 1/04/2020
+    }
+}
