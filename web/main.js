317be2934d9115e6ae6bf547937225d8a3c966f5
Anthony Marshall <anthonymarshall@gmail.com>
2020-04-08 15:10:28 +1200

    refactor pace calculation

--- web/main.js
+++ web/main.js
@@ -58,27 +58,11 @@ function viewActivities() {
 }
 
 function speedToPace(speed){
-    return fancyTimeFormat(1000/speed)
+    return  moment.utc((1000/speed)*1000).format('mm:ss');
+
 }
 
-function fancyTimeFormat(time)
-{
-    // Hours, minutes and seconds
-    var hrs = ~~(time / 3600);
-    var mins = ~~((time % 3600) / 60);
-    var secs = ~~time % 60;
 
-    // Output like "1:01" or "4:03:59" or "123:03:59"
-    var ret = "";
-
-    if (hrs > 0) {
-        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-    }
-
-    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    ret += "" + secs;
-    return ret;
-}
 
 let dataJson = null;
 
